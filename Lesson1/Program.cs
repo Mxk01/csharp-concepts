d6ab10f56c6ca5f6b512fa07ea9c72d1058dc2a1
Mxk01 <RLuntraru22@gmail.com>
2022-12-10 11:41:01 +0200

    =

---
+++ Lesson1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+
+
+object a = 100;
+dynamic b = "hello";
+
+
+Console.WriteLine(b.GetType());
+Console.WriteLine(a.GetType());
+
+
+P1 test = new P1();
+test.FirstMethod();
+
+
+P1.LogContentAndType(b);
+
+String myString = "";
+myString.Concat("Abc");
+P1.LogContentAndType(myString);
+class P1
+{
+    public  void FirstMethod()
+    {
+        Console.WriteLine("Something");
+        Console.WriteLine("Size of int is {0} bytes and size of float is {1} bytes", sizeof(int),sizeof(float));
+    }
+    public static void LogContentAndType(dynamic myVar)
+    {
+        Console.WriteLine($"{myVar} {myVar.GetType()}");
+        
+    }
+}
+
+ 
