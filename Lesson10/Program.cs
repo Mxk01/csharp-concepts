01199ab9de4b3b198eb7c74a34d88921c9c34dbd
Mxk01 <RLuntraru22@gmail.com>
2022-12-15 23:16:33 +0200

    =

---
+++ Lesson10/Program.cs
@@ -0,0 +1,15 @@
+﻿#define PI 
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#if (PI)
+Console.WriteLine("PI is defined");
+#elif (PI && !VCC) 
+Console.WriteLine("VCC isn't defined but PI is");
+#else
+Console.WriteLine("PI isn't defined");
+#endif
